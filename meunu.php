7383737321140d0ea268bf6082f2708fab174792
santiago <106820207+santiagoo4512@users.noreply.github.com>
2024-08-02 11:56:51 -0400

    Add files via upload

---
+++ meunu.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registro de oficina</title>
+</head>
+<body>
+    <label for="ciudad">ciudad:</label>
+    <input type="text">
+    <hr>
+    <label for="pais">pais:</label>
+    <input type="text">
+    <hr>
+    <label for="region">region:</label>
+    <input type="text">
+    <hr>
+    <label for="coPostal">coPostal:</label>
+    <input type="text">
+    <hr>
+    <label for="telefono">telefono:</label>
+    <input type="text">
+    <hr>
+    <label for="liDireccion">liDireccion:</label>
+    <input type="text">
+    <hr>
+    <label for="liDireccion2">liDireccion2:</label>
+    <input type="text">
+    <hr>
+    <button type="submit">Registrar</button>
+    
+
+</body>
+</html>
